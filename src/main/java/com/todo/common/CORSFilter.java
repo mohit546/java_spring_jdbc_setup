6e977d9496baa967e20020da9fef1a908bbfe07e
Mohit Patil <mohit.patil@vertoz.com>
2018-07-20 14:01:05 +0530

    - CORS filter added

---
+++ src/main/java/com/todo/common/CORSFilter.java
@@ -0,0 +1,32 @@
+package com.todo.common;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class CORSFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String origin = request.getHeader("origin");
+        origin = (origin == null || origin.equals("")) ? "null" : origin;
+        response.addHeader("Access-Control-Allow-Origin", origin);
+        response.addHeader("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, DELETE, OPTIONS");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        response.addHeader("Access-Control-Allow-Headers",
+                "Authorization, origin, content-type, accept, x-requested-with");
+        
+        System.out.println(request.getHeader("content-type"));
+        System.out.println(response);
+        filterChain.doFilter(request, response);
+	}
+	
+}
